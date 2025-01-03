37c6b4b49ef39431a50b218ad3b28d012493c48a
echothedeveloper <admin@echothedeveloper.com>
2025-01-03 19:21:44 +0000

    Working on Updater

--- src/main/java/com/echothedeveloper/echolang/interpreter/shell/Updater.java
+++ src/main/java/com/echothedeveloper/echolang/interpreter/shell/Updater.java
@@ -1,6 +1,11 @@
 package com.echothedeveloper.echolang.interpreter.shell;
 
 public class Updater {
+    String LATEST_VERSION_URL[] = {
+            "https://raw.githubusercontent.com/EchoTheDeveloper/echolang/refs/heads/master/version/major",
+            "https://raw.githubusercontent.com/EchoTheDeveloper/echolang/refs/heads/master/version/minor",
+            "https://raw.githubusercontent.com/EchoTheDeveloper/echolang/refs/heads/master/version/patch"
+    };
     public static void checkUpdates() {
 
     }
